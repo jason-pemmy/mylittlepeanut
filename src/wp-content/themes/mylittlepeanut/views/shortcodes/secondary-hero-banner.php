698da9f816dd5372a5c0cdcd1678ca6e8e9bec58
Jason Pembleton CW <A6CH1ZZ@usac.mmm.com>
2016-10-13 11:16:44 -0400

    wer

---
+++ src/wp-content/themes/mylittlepeanut/views/shortcodes/secondary-hero-banner.php
@@ -0,0 +1,6 @@
+<div class="hero-banner-container" style="background-image:url('<?php echo $image;?>')">
+	<div class="hero-banner-text">
+		<div class="hero-heading"><?php echo $heading; ?></div>
+        <div class="hero-copy"><?php echo $copy; ?></div>
+	</div>
+</div>
